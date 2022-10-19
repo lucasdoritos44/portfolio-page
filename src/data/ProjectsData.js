e3d9b2dc9f55e2adb7aebc6d9dd9211d8e52bf19
Łukasz Jesionowski <lukaszjesionowski44@gmail.com>
2022-10-19 11:20:49 +0200

    changed project

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -19,7 +19,7 @@ export const projects = [
         link: 'https://dailycryptoapp.netlify.app',
         link__github: 'https://github.com/lucasdoritos44/dailyCrypto',
          name: 'Daily Crypto',
-         description: 'Daily Crypto is a website where you can find important information about Bitcoin, Ethereum and other cryptocurrencies, such as price charts, price history or useful links to dive deep into blockchain technology. Daily Crypto also brings you daily news about blockchain. In addition to cryptocurrencies, you can find here the top 100 exchanges where you can buy and hold your cryptocurrencies.',
+         description: 'Daily Crypto is a fully responsive website where you can find important information about Bitcoin, Ethereum and other cryptocurrencies, such as price charts, price history or useful links to dive deep into blockchain technology. Daily Crypto also brings you daily news about blockchain. In addition to cryptocurrencies, you can find here the top 100 exchanges where you can buy and hold your cryptocurrencies.',
          tools: 'HTML, CSS, Javascript, React, React-chartjs, Axios, CoinGecko API, Material-UI', 
          video: DailyCryptoVideo   
      },
